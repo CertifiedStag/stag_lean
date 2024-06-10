03367f21dd33b93063896eb4232de6912d61f70c
CertifiedStag <107716735+CertifiedStag@users.noreply.github.com>
2024-06-10 08:24:06 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,18 @@
-Config = {}
+Config = {
 
 -- Here you can change the prop used for the table.
-Config.tableProp = 'v_ret_ml_tableb'
+tableProp = 'v_ret_ml_tableb',
 
 -- Here you can change the location of the table, don't forget to change the heading as well.
-Config.tableLoc = vector3(473.58, -629.74, 23.91)
-Config.heading = 0.0
+tableLoc = { x = 473.58, y = -629.74, z = 23.91},
+heading = 0.0,
 
 -- Here you can change the amount of ingredients required and how much Lean you recieve on completion.
-Config.CSAmount = 1 -- Cough Syrup
-Config.ANAmount = 1 -- Antihistemine
-Config.KKAmount = 1 -- KurkaKola
-Config.recieveAmount = 1 -- Lean amount
+CSAmount = 1, -- Cough Syrup
+ANAmount = 1, -- Antihistemine
+KKAmount = 1, -- KurkaKola
+recieveAmount = 1, -- Lean amount
 
 -- Change how long it takes to create Lean
-Config.creationTime = 5000
+creationTime = 5000
+}
