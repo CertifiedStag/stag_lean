8e2908bf87fffcb6cdb168ae1bdf611190beb0b5
CertifiedStag <107716735+CertifiedStag@users.noreply.github.com>
2024-06-10 21:49:55 +0100

    Update sv_lean.lua

--- sv_lean.lua
+++ sv_lean.lua
@@ -4,15 +4,13 @@ RegisterServerEvent('stag_lean:server:MakeLean')
 AddEventHandler('stag_lean:server:MakeLean', function()
     local Player = QBCore.Functions.GetPlayer(source)
     local hasItem = Player.Functions.GetItemByName('coughsyrup')
-    local hasItem2 = Player.Functions.GetItemByName('fizzycola')
+    local hasItem2 = Player.Functions.GetItemByName('kurkakola')
     local hasItem3 = Player.Functions.GetItemByName('antihistemine')
-    local hasItem4 = Player.Functions.GetItemByName('leancup')
-    if hasItem and hasItem2 and hasItem3 and hasItem4 then
+    if hasItem and hasItem2 and hasItem3 then
         -- Remove the items required to make lean
         Player.Functions.RemoveItem('coughsyrup', 1)
-        Player.Functions.RemoveItem('fizzycola', 1)
+        Player.Functions.RemoveItem('kurkakola', 1)
         Player.Functions.RemoveItem('antihistemine', 1)
-        Player.Functions.RemoveItem('leancup', 1)
         -- Add the lean item
         Player.Functions.AddItem('lean', 1)
         
